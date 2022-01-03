dbe8f238072f4aba67689afbd2821bbfb96a9ee3
Apoorva Sambhav <apoorva4sambhav@gmail.com>
2022-01-02 20:47:57 -0500

    second commit

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # About script
-The script is created via bash. This is used to determine/check which ports are open on the current machine.
+The script is created via bash. This is used to determine/check which ports are open on any target specififed.
 
 ```
 To start scanning to see which ports are open, do the following.
 
-Syntax: port-list.sh 22,443,...
+Syntax: ./port-list.sh -t '10.0.0.1' 22,443 or ./port-list.sh -a '10.0.0.1'
 
 
       -h     For help or detail on how to use the command.
       -a     For performing a scan on all ports from 0-65535.
+      -t     Specify the target.
 ```
--- port-list.sh
+++ port-list.sh
@@ -8,16 +8,17 @@ Help()
 {
     # Help message
     echo -e "To start scanning to see which ports are open, do the following. \n"
-    echo -e "Syntax: port-list.sh 22,443,...\n\n"
+    echo -e "Syntax: ./port-list.sh -t '10.0.0.1' 22,443 or ./port-list.sh -a '10.0.0.1' \n\n"
     echo "      -h     For help or detail on how to use the command."
     echo "      -a     For performing a scan on all ports from 0-65535."
+    echo "      -t     Specify the target."
 }
 
 All()
 {
     # Port scan from 0-65535
     for port in {0..65535}; do
-        scan_all=$(echo 2>&1 >/dev/tcp/localhost/$port)
+        scan_all=$(echo 2>&1 >/dev/tcp/$host/$port)
         if [[ -z "$scan_all" ]]; then
             echo "$port is open."
         else
@@ -26,30 +27,39 @@ All()
     done
 }
 
+
 # Main
 
-while getopts ":ha" option; do
+# Set Variable
+host="localhost"
+
+while getopts ":h:t:a:" option; do
    case $option in
       h) # display Help
-      Help
-      exit;;
+        Help
+        exit;;
+      t) # Specify host(s)
+        host=$OPTARG
+        echo For the host $host;;
       a) # Scans all ports
-      All
-      exit;;
+        host=$OPTARG
+        echo For the host $host
+        All
+        exit;;
       \?) # Invalid option
-      echo "Error: Invalid option. Use '-h' option to see how to use the shell script."
-      exit;;
+        echo "Error: Invalid option. Use '-h' option to see how to use the shell script."
+        exit;;
    esac
 done
 
 # Working with arrays in bash https://opensource.com/article/18/5/you-dont-know-bash-intro-bash-arrays. And replacing without using sed https://stackoverflow.com/a/30021851.
-ports=$1
+ports=$3
 ports="${ports//,/ }"
 # echo $ports
 
 for port_number in ${ports[@]}; do
     # If there is no stderr then port is open. https://stackoverflow.com/a/12137501. How to pipe stderr to stdout https://stackoverflow.com/a/2342841.
-    cmd=$(echo 2>&1 >/dev/tcp/localhost/$port_number)
+    cmd=$(echo 2>&1 >/dev/tcp/$host/$port_number)
     # Bash conditional Expressions https://stackoverflow.com/a/30021851.
     if [[ -z "$cmd" ]]; then
         echo "$port_number is open."
